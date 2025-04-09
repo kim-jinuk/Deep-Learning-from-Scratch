3e1b0aaa7a3a0ea876a475b5f266bb58ae123b4a
JINUK\kjm88 <gjm8856@gmail.com>
2025-04-09 15:27:23 +0900

    2.5.multi_perceptron.py create

---
+++ ch2/2.5.multi_perceptron.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+# 2.3.1 AND 게이트 구현
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(x*w) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+# 2.3.3 NAND, OR 게이트 구현
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(x*w) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(x*w) + b
+    if tmp < 0:
+        return 0
+    else:
+        return 1
+    
+
+# 2.5.2 XOR 게이트 구현하기 (다층 퍼셉트론)
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
