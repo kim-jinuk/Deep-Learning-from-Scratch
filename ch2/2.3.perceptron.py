4d29fdabc087f790794d418395478d2b28057799
JINUK\kjm88 <gjm8856@gmail.com>
2025-04-09 14:19:02 +0900

    2.3.perceptron.py create

---
+++ ch2/2.3.perceptron.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+# 2.3.1 AND 게이트 구현
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(x*w) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+# 2.3.3 NAND, OR 게이트 구현
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(x*w) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(x*w) + b
+    if tmp < 0:
+        return 0
+    else:
+        return 1
